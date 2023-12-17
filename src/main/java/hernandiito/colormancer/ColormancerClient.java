d0fb60ab823f20e1143eb3db6d092b81e593266d
HERNANDIITO <pablohhgg@gmail.com>
2023-12-17 21:11:21 +0100

    set up

---
+++ src/main/java/hernandiito/colormancer/ColormancerClient.java
@@ -0,0 +1,10 @@
+package hernandiito.colormancer;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ColormancerClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
